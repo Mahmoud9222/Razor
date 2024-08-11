0f6a613bed64ed8364f79b24a618799f2cd1719a
Mahmoud <112482449+Mahmoud9222@users.noreply.github.com>
2024-08-11 22:57:03 +0200

    Add files via upload

---
+++ WebApp/Data/Migrations/20240811130340_ColumnsAdded.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class ColumnsAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            //migrationBuilder.AddColumn<string>(
+            //    name: "FirstName",
+            //    table: "AspNetUsers",
+            //    type: "nvarchar(max)",
+            //    nullable: false,
+            //    defaultValue: "");
+
+            //migrationBuilder.AddColumn<string>(
+            //    name: "LastName",
+            //    table: "AspNetUsers",
+            //    type: "nvarchar(max)",
+            //    nullable: false,
+            //    defaultValue: "");
+        }
+
+    }
+}
